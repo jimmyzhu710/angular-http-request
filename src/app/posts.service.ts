1a18df04128b5666b2e4015a11e26db4eb8a171e
Jimmy <jzhu@choice.com.au>
2020-05-16 08:22:43 +1000

    264. Using Subjects for Error Handling

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,23 +1,41 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
-import { Post } from './post.model';
+import { Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
+
+import { Post } from './post.model';
 
 @Injectable({
     providedIn: 'root'
 })
 
 export class PostService {
+    error = new Subject<string>();
+
     constructor(private http: HttpClient) { }
- 
+
+    // createAndStorePost(title: string, content: string) {
+    //     const postData: Post = { title: title, content: content };
+    //     // Send Http request
+    //     return this.http.post<{ name: string }>(
+    //         'https://angular-http-requests-6bec1.firebaseio.com/posts.json',
+    //         postData
+    //     );
+    // }     
+
     createAndStorePost(title: string, content: string) {
         const postData: Post = { title: title, content: content };
         // Send Http request
-        return this.http.post<{ name: string }>(
+        this.http.post<{ name: string }>(
             'https://angular-http-requests-6bec1.firebaseio.com/posts.json',
             postData
+        ).subscribe(responseData => {
+            console.log(responseData);
+        }, error => {
+            this.error.next(error.message);
+        }
         );
-    }     
+    }
 
 
     fetchPosts() {
@@ -33,7 +51,7 @@ export class PostService {
             }));
     }
 
-    deletePosts(){
-       return this.http.delete('https://angular-http-requests-6bec1.firebaseio.com/posts.json');
+    deletePosts() {
+        return this.http.delete('https://angular-http-requests-6bec1.firebaseio.com/posts.json');
     }
 }
